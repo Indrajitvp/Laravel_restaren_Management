0673bc4083072f62a5742bdf0f2202e71c01f69e
Indrajit <indrajitvp27@gmail.com>
2025-07-27 00:11:49 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,6 +1,5 @@
 #!/usr/bin/env bash
-
-# Run Laravel migrations and serve
 php artisan migrate --force
-php artisan serve --host 0.0.0.0 --port 10000
-
+php artisan config:cache
+php artisan route:cache
+php artisan serve --host=0.0.0.0 --port=10000
